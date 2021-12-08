a2ec732d848afca4e37f181d4a6e570d5e0b535e
Artem <lukart80@yandex.ru>
2021-12-08 09:11:54 +0300

    Создана модель Tag

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,8 +4,16 @@ from django.db import models
 class Ingredient(models.Model):
     """Модель для ингредиента."""
 
-    name = models.CharField(max_length=100, verbose_name='Название ингредиента')
-    measurement_unit = models.CharField(max_length=100, verbose_name='Единицы измерения')
+    name = models.CharField(max_length=100, verbose_name='Название ингредиента', blank=False, null=False)
+    measurement_unit = models.CharField(max_length=100, verbose_name='Единицы измерения', blank=False, null=False)
 
     class Meta:
         ordering = ('name',)
+
+
+class Tag(models.Model):
+    """Модель для тегов."""
+
+    name = models.CharField(max_length=200, verbose_name='Название', blank=False, null=False)
+    color = models.CharField(max_length=7, verbose_name='Код цвета')
+    slug = models.SlugField(max_length=200)
