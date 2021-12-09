746dcc4d3a19d2ceebb9bcbbdd23ad87902eab84
Artem <lukart80@yandex.ru>
2021-12-09 11:19:48 +0300

    Отображается количество ингредиентов

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from .models import Ingredient, Tag, Recipe
+from .models import Ingredient, Tag, Recipe, IngredientAmount
 from users.serializers import UserSerializer
 
 
@@ -20,16 +20,40 @@ class TagSerializer(ModelSerializer):
         fields = ('id', 'name', 'color', 'slug')
 
 
-class RecipeSerializer(ModelSerializer):
-    """Сериализотор для рецептов."""
+class IngredientAmountSerializer(ModelSerializer):
+    """Сериализатор промежуточной модели IngredientQuantity."""
+    id = serializers.SerializerMethodField(read_only=True)
+    name = serializers.SerializerMethodField(read_only=True)
+    measurement_unit = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        fields = ('id', 'name', 'amount', 'measurement_unit')
+        model = IngredientAmount
+
+    def get_id(self, obj):
+        return obj.ingredient.id
+
+    def get_name(self, obj):
+        return obj.ingredient.name
+
+    def get_measurement_unit(self, obj):
+        return obj.ingredient.measurement_unit
+
+
+class RecipeReadSerializer(ModelSerializer):
+    """Сериализотор для получения рецептов."""
     image = serializers.SerializerMethodField()
     tags = TagSerializer(many=True)
-    ingredients = IngredientSerializer(many=True)
+    ingredients = serializers.SerializerMethodField(read_only=True)
     author = UserSerializer()
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'name', 'image', 'text', 'id', 'ingredients', 'cooking_time')
+        fields = ('tags', 'author', 'name', 'image', 'text', 'id', 'ingredients', 'cooking_time',)
+        read_only_fields = ['tags', 'author', 'name', 'image', 'text', 'id', 'ingredients', 'cooking_time']
 
     def get_image(self, obj):
         return obj.image.url
+
+    def get_ingredients(self, obj):
+        return IngredientAmountSerializer(obj.recipe_amount.all(), many=True).data
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
-from .models import Ingredient, Tag, Recipe
-from .serializers import IngredientSerializer, TagSerializer, RecipeSerializer
+from .models import Ingredient, Tag, Recipe, IngredientAmount
+from .serializers import IngredientSerializer, TagSerializer, RecipeReadSerializer
 from .filters import IngredientFilter
 from .pagination import RecipePagination
 
@@ -18,7 +18,14 @@ class TagViewSet(ReadOnlyModelViewSet):
     serializer_class = TagSerializer
 
 
+
+
+
 class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
+
     pagination_class = RecipePagination
+
+    def get_serializer_class(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return RecipeReadSerializer
